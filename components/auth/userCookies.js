8e1233b5843d31adaaa6ecc1fb120f8e4a0888c9
betulkaplan <betulkaplan7@gmail.com>
2021-06-25 19:25:59 +0300

    userCookies updated

--- components/auth/userCookies.js
+++ components/auth/userCookies.js
@@ -10,8 +10,10 @@ export const getUserFromCookie = () => {
 
 export const setUserCookie = (user) => {
   cookies.set('auth', user, {
+    // firebase id tokens expire in one hour
+    // set cookie expiry to match
     expires: 1 / 24,
   });
 };
 
-export const removeUserCookie = () => cookies.removeItem('auth');
+export const removeUserCookie = () => cookies.remove('auth');
